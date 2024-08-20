235e747bc377a7352f5f0260271786b594859b2e
Alisotoudehpsn4 <alisotoudehpsn4@gmail.com>
2024-08-20 14:18:32 +1000

    added category buttons as well

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 import BigButton from './components/BigButton';
+import Categories from './components/Categories';
 
 function App() {
   return (
     <div className="App">
 
         <BigButton />
+        <br/> 
+        <Categories />
     </div>
   );
 }
---
+++ src/components/Categories.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Categories = () => {
+
+
+  return (
+    <div>
+      <button>Staff</button>
+      <button>Material</button>
+      <button>Location</button>
+      <button>Job-Material</button>
+      <button>Job-Activity</button>
+      <button>Job</button>
+      <button>DB-Note</button>
+      <button>Company</button>
+      <button>Attachments</button>
+    </div>
+  )
+}
+
+export default Categories
